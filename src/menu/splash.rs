e706fd9e3aa7f806eea19b025f4aed177c33cbb5
Ian <ian524580@gmail.com>
2023-06-03 12:10:03 -0700

    Update splash.rs

--- src/menu/splash.rs
+++ src/menu/splash.rs
@@ -1,18 +1,13 @@
 use bevy::prelude::*;
 use crate::prelude::*;
 
-// This plugin will display a splash screen with Bevy logo for 1 second before switching to the menu
 pub struct SplashPlugin;
 
 impl Plugin for SplashPlugin {
     fn build(&self, app: &mut App) {
-        // As this plugin is managing the splash screen, it will focus on the state `GameState::Splash`
         app
-            // When entering the state, spawn everything needed for this screen
             .add_system(splash_setup.in_schedule(OnEnter(GameState::Splash)))
-            // While in this state, run the `countdown` system
             .add_system(countdown.in_set(OnUpdate(GameState::Splash)))
-            // When exiting the state, despawn everything that was spawned for this screen
             .add_systems((
                 cleanup::<Camera>.in_schedule(OnExit(GameState::Splash)),
                 cleanup::<OnSplashScreen>.in_schedule(OnExit(GameState::Splash)),
@@ -20,18 +15,15 @@ impl Plugin for SplashPlugin {
     }
 }
 
-// Tag component used to tag entities added on the splash screen
 #[derive(Component)]
 struct OnSplashScreen;
 
-// Newtype to use a `Timer` for this screen as a resource
 #[derive(Resource, Deref, DerefMut)]
 struct SplashTimer(Timer);
 
 fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let icon = asset_server.load("branding/icon.png");
     commands.spawn(Camera2dBundle::default()).insert(Reboot);
-    // Display the logo
     commands
         .spawn((
             NodeBundle {
@@ -48,7 +40,6 @@ fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         .with_children(|parent| {
             parent.spawn(ImageBundle {
                 style: Style {
-                    // This will set the logo to be 200px wide, and auto adjust its height
                     size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                     ..default()
                 },
@@ -56,11 +47,9 @@ fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 ..default()
             });
         });
-    // Insert the timer as a resource
     commands.insert_resource(SplashTimer(Timer::from_seconds(2.0, TimerMode::Once)));
 }
 
-// Tick the timer, and change state when finished
 fn countdown(
     mut game_state: ResMut<NextState<GameState>>,
     time: Res<Time>,
